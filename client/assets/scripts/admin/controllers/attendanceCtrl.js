7998b86936db897d4df718030eda4996c61e94cc
Dane Smith <danesmith@Tiffany-Renandos-MacBook-Air.local>
2015-12-15 15:53:05 -0600

    merge

--- client/assets/scripts/admin/controllers/attendanceCtrl.js
+++ client/assets/scripts/admin/controllers/attendanceCtrl.js
@@ -1,3 +1,6 @@
 myApp.controller('attendanceCtrl', ['$scope', '$http', function($scope, $http){
-    console.log('on admin attendance controller--attendanceCtrl.js')
+    console.log('on admin attendance controller--attendanceCtrl.js');
+
+
+
 }]);
