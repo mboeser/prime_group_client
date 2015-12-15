a0c21102031fe978d75fa051166f9e82a2c540fc
Mike Boeser <mboeser@yahoo.com>
2015-12-15 11:15:38 -0600

    update login

--- server/config/passport.js
+++ server/config/passport.js
@@ -1,5 +1,4 @@
 // load all the things we need
-//var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 // load up the user model
@@ -17,20 +16,12 @@ module.exports = function (passport) {
     // passport needs ability to serialize and deserialize users out of session
 
     // used to serialize the user for the session
-    //passport.serializeUser(function (user, done) {
-    //    done(null, user.id);
-    //});
-    //
-    //// used to deserialize the user
-    //passport.deserializeUser(function (id, done) {
-    //    User.findById(id, function (err, user) {
-    //        done(err, user);
-    //    });
-    //});
 
     passport.serializeUser(function(user, done) {
         done(null, user);
     });
+
+    //// used to deserialize the user
 
     passport.deserializeUser(function(obj, done) {
         done(null, obj);
@@ -43,77 +34,9 @@ module.exports = function (passport) {
 
             clientID: process.env.CLIENT_ID || configAuth.googleAuth.clientID,
             clientSecret: process.env.CLIENT_SECRET || configAuth.googleAuth.clientSecret,
-            callbackURL: configAuth.googleAuth.callbackURL,
-            //passReqToCallback: true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
+            callbackURL: configAuth.googleAuth.callbackURL
 
         },
-        //function (req, token, refreshToken, profile, done) {
-        //
-        //    // asynchronous
-        //    process.nextTick(function () {
-        //
-        //        // check if the user is already logged in
-        //        if (!req.user) {
-        //
-        //            User.findOne({'google.id': profile.id}, function (err, user) {
-        //                if (err)
-        //                    return done(err);
-        //
-        //                if (user) {
-        //
-        //                    // if there is a user id already but no token (user was linked at one point and then removed)
-        //                    if (!user.google.token) {
-        //                        user.google.token = token;
-        //                        user.google.name = profile.displayName;
-        //                        user.google.email = (profile.emails[0].value || '').toLowerCase(); // pull the first email
-        //
-        //                        user.save(function (err) {
-        //                            if (err)
-        //                                return done(err);
-        //
-        //                            return done(null, user);
-        //                        });
-        //                    }
-        //
-        //                    return done(null, user);
-        //                } else {
-        //                    var newUser = new User();
-        //
-        //                    newUser.google.id = profile.id;
-        //                    newUser.google.token = token;
-        //                    newUser.google.name = profile.displayName;
-        //                    newUser.google.email = (profile.emails[0].value || '').toLowerCase(); // pull the first email
-        //
-        //                    newUser.save(function (err) {
-        //                        if (err)
-        //                            return done(err);
-        //
-        //                        return done(null, newUser);
-        //                    });
-        //                }
-        //            });
-        //
-        //        } else {
-        //            // user already exists and is logged in, we have to link accounts
-        //            var user = req.user; // pull the user out of the session
-        //
-        //            user.google.id = profile.id;
-        //            user.google.token = token;
-        //            user.google.name = profile.displayName;
-        //            user.google.email = (profile.emails[0].value || '').toLowerCase(); // pull the first email
-        //
-        //            user.save(function (err) {
-        //                if (err)
-        //                    return done(err);
-        //
-        //                return done(null, user);
-        //            });
-        //
-        //        }
-        //
-        //    });
-        //}));
-
 
     function(accessToken, refreshToken, profile, done) {
         // asynchronous verification, for effect...
