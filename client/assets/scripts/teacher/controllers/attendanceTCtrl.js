e8c37624fe469a4b4c1f20b6a6a80ea2cc190dcf
Dane Smith <danesmith@Tiffany-Renandos-MacBook-Air.local>
2015-12-16 14:21:50 -0600

    more controllwer work

--- client/assets/scripts/teacher/controllers/attendanceTCtrl.js
+++ client/assets/scripts/teacher/controllers/attendanceTCtrl.js
@@ -1,3 +1,17 @@
 myApp.controller('attendanceTCtrl', ['$scope', '$http', function($scope, $http){
-    console.log('on teacher attendance controller--attendanceTCtrl.js')
+    console.log('on teacher attendance controller--attendanceTCtrl.js');
+
+
+    $scope.dataService = DataService;
+    $scope.user = {};
+    $scope.user = $scope.dataService.peopleData();
+
+    if($scope.dataService.peopleData() === undefined){
+        $scope.dataService.retrieveData().then(function(){
+            $scope.user = $scope.dataService.peopleData();
+        });
+    }
+
+
+
 }]);
