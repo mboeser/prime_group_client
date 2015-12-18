9a87dee62252ba018182bc423b59b377cfb456a1
User <user@Kates-MacBook-Pro.local>
2015-12-18 16:01:01 -0600

    more pg calls

--- server/routes/student.js
+++ server/routes/student.js
@@ -77,6 +77,20 @@ module.exports = function (app, req, res, next) {
             });
         });
 
+        app.put('/updateStudentInfo', isLoggedIn, function(req, res){
+            var column = req.body['params']['column'];
+            var value = req.body['params']['value'];
+            var id = req.body['params']['id'];
+
+            pg.connect(connectionString.url, function (err, client, done) {
+                client.query("UPDATE students SET " + column + "=$1 WHERE id=$2", [value, id], function (err) {
+                    if (err) console.log(err);
+                    client.end();
+                    res.send(true);
+                });
+            });
+        });
+
     });
 };
 
