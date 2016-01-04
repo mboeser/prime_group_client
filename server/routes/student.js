e239916976c8321cde0a559f6dc6ffc24a7e64de
Dane Smith <danesmith@Tiffany-Renandos-MacBook-Air.local>
2016-01-04 16:22:49 -0600

    fix updateBus

--- server/routes/student.js
+++ server/routes/student.js
@@ -67,37 +67,38 @@ module.exports = function (app, req, res, next) {
                 res.send(true);
             });
         });
-
-        app.put('/updateBus', isLoggedIn, function(req, res){
-            var column = req.body['params']['column'];
-            var value = req.body['params']['value'];
-            var id = req.body['params']['id'];
-
-            pg.connect(connectionString.url, function (err, client, done) {
-                client.query("UPDATE busses SET " + column + "=$1 WHERE id=$2", [value, id], function (err) {
-                    if (err) console.log(err);
-                    client.end();
-                    res.send(true);
-                });
-            });
-        });
-
-        app.put('/updateStudentInfo', isLoggedIn, function(req, res){
-            var column = req.body['params']['column'];
-            var value = req.body['params']['value'];
-            var id = req.body['params']['id'];
-
-            pg.connect(connectionString.url, function (err, client, done) {
-                client.query("UPDATE students SET " + column + "=$1 WHERE id=$2", [value, id], function (err) {
-                    if (err) console.log(err);
-                    client.end();
-                    res.send(true);
-                });
-            });
-        });
-
-
     });
+
+    app.put('/updateBus', isLoggedIn, function(req, res){
+        var column = req.body['params']['column'];
+        var value = req.body['params']['value'];
+        var id = req.body['params']['id'];
+
+        pg.connect(connectionString.url, function (err, client, done) {
+            client.query("UPDATE busses SET " + column + "=$1 WHERE id=$2", [value, id], function (err) {
+                if (err) console.log(err);
+                client.end();
+                res.send(true);
+            });
+        });
+    });
+
+    app.put('/updateStudentInfo', isLoggedIn, function(req, res){
+        var column = req.body['params']['column'];
+        var value = req.body['params']['value'];
+        var id = req.body['params']['id'];
+
+        pg.connect(connectionString.url, function (err, client, done) {
+            client.query("UPDATE students SET " + column + "=$1 WHERE id=$2", [value, id], function (err) {
+                if (err) console.log(err);
+                client.end();
+                res.send(true);
+            });
+        });
+    });
+
+
+
 };
 
 // route middleware to ensure user is logged in
