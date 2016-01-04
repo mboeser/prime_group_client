c6e5262621a87cd35e7d1381418cfcfff00ad29c
User <user@Kates-MacBook-Pro.local>
2016-01-04 15:55:14 -0600

    homework sent and excused buttons admin side working

--- client/assets/scripts/admin/controllers/absentCtrl.js
+++ client/assets/scripts/admin/controllers/absentCtrl.js
@@ -6,6 +6,7 @@ myApp.controller('absentCtrl', ['$scope', '$http', 'DataService', function($scop
     $scope.date = $scope.dataService.getDate();
 
     $scope.user = $scope.dataService.peopleData();
+    $scope.homework_sent = false;
 
     if($scope.dataService.peopleData() === undefined){
         $scope.dataService.retrieveData().then(function(){
@@ -18,8 +19,8 @@ myApp.controller('absentCtrl', ['$scope', '$http', 'DataService', function($scop
         '<div ng-if="!row.entity.contact_status">Not Yet Called</div>';
 
 
-    var excusedCheckbox = "<md-checkbox ng-model='row.entity.excused' ng-true-value='true' ng-false-value='false' class='md-warn md-hue2' type='checkbox' name='excused' ></md-checkbox>"; 
-    var homeworkCheckbox = "<md-checkbox  ng-model='row.entity.homework_sent' ng-true-value='true' ng-false-value='false' class='md-warn md-hue2' type='checkbox' name='homework_sent' ></md-checkbox>";
+    var excusedCheckbox = "<md-checkbox ng-model='row.entity.excused' class='md-warn md-hue2' type='checkbox' name='excused' ng-change='grid.appScope.saveRow(row.entity)'></md-checkbox>"; 
+    var homeworkCheckbox = "<md-checkbox ng-model='row.entity.homework_sent' class='md-warn md-hue2' type='checkbox' name='homework_sent'  ng-change='grid.appScope.saveRow(row.entity)'></md-checkbox>";
 
 
     $scope.gridOptions = {
@@ -49,8 +50,11 @@ myApp.controller('absentCtrl', ['$scope', '$http', 'DataService', function($scop
 
 
     $scope.saveRow = function( rowEntity ) {
+        console.log("in save row");
+        console.log("rowEntity", rowEntity);
         var promise = $http.put('/updateAbsent', rowEntity).then(function(response){
             console.log(response);
+            console.log(promise);
         });
         $scope.gridApi.rowEdit.setSavePromise( rowEntity, promise);
     };
@@ -69,5 +73,7 @@ myApp.controller('absentCtrl', ['$scope', '$http', 'DataService', function($scop
 
         });
     }
+
+
 
 }]);
