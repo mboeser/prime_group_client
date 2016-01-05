d51beb2e1c7bdc5601c1694522041f2c005fb946
User <user@Kates-MacBook-Pro.local>
2016-01-05 14:03:51 -0600

    changes to student prework shows in class list

--- client/assets/scripts/teacher/controllers/studentCtrl.js
+++ client/assets/scripts/teacher/controllers/studentCtrl.js
@@ -8,6 +8,7 @@ myApp.controller('studentCtrl', ['$scope', '$http', 'DataService', function ($sc
     $scope.user = $scope.dataService.peopleData();
     $scope.student = $scope.dataService.getStudent();
     console.log("This is the student that was selected", $scope.student);
+    console.log("Date upon entry", $scope.date);
 
     if($scope.dataService.peopleData() === undefined){
         $scope.dataService.retrieveData().then(function(){
@@ -21,20 +22,30 @@ myApp.controller('studentCtrl', ['$scope', '$http', 'DataService', function ($sc
     $scope.updateNotes = function(col, note){
         $http.put('/updateStudent', {params: {'column': col, 'value': note, 'id': $scope.student.id}}).then(function(){
             console.log("student data updated");
+            $scope.setPrework($scope.date);
         });
     };
 
     $scope.updateBus = function(col, note){
       $http.put('/updateBus', {params: {'column': col, 'value': note, 'id': $scope.student.id}}).then(function(){
           console.log("student data updated");
+          $scope.setPrework($scope.date);
       });
     };
 
     $scope.updateInfo = function(col, note){
         $http.put('/updateStudentInfo', {params: {'column': col, 'value': note, 'id': $scope.student.id}}).then(function(){
             console.log("student data updated");
+            $scope.setPrework($scope.date);
         });
     };
 
     //before switching to the next page, don't forget to update the student factory information?
+    $scope.setPrework = function(date) {
+        console.log("This is date", date);
+        $http.get('/teacher_prework', {params: {'date': date}}).then(function (response) {
+            $scope.dataService.setData(response.data);
+        });
+    }
+
 }]);
