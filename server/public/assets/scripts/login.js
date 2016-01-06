9f474b809bf258b3541f65ba083d7beef327b945
doctorhowser <doctorhowser@gmail.com>
2016-01-06 17:20:06 -0600

    fix gitignore

---
+++ server/public/assets/scripts/login.js
@@ -0,0 +1,5 @@
+/**
+ * Created by mkboeser on 12/11/15.
+ */
+var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial', 'ngMessages']);
+
---
+++ server/public/assets/scripts/loginCtrl.js
@@ -0,0 +1,3 @@
+myApp.controller('loginCtrl', ['$scope', '$http', function($scope, $http){
+    console.log('on Login controller--loginCtrl.js')
+}]);
